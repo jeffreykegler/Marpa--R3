f7db1bbf06132af81a3f694e28e53a55a38e8a4f
Jeffrey Kegler <jeffreykegler@jeffreykegler.com>
2016-06-05 13:04:21 -0700

    Adding Lua-powered semantics: t++

---
+++ cpan/lib/Marpa/R3/Lua.pm
@@ -0,0 +1,80 @@
+# Marpa::R3 is Copyright (C) 2016, Jeffrey Kegler.
+#
+# This module is free software; you can redistribute it and/or modify it
+# under the same terms as Perl 5.10.1. For more details, see the full text
+# of the licenses in the directory LICENSES.
+#
+# This program is distributed in the hope that it will be
+# useful, but it is provided “as is” and without any express
+# or implied warranties. For details, see the full text of
+# of the licenses in the directory LICENSES.
+
+package Marpa::R3::Lua;
+
+use 5.010001;
+use warnings;
+use strict;
+
+use vars qw($VERSION $STRING_VERSION);
+$VERSION        = '4.001_010';
+$STRING_VERSION = $VERSION;
+$VERSION        = eval $VERSION;
+
+# Lua methods
+
+# These can, and probably should, be pre-compiled someday,
+# reducing start-up time.
+
+$Marpa::R3::Lua::value_init = <<'END_OF_LUA';
+    -- for k,v in pairs(marpa.ops)
+    -- do io.stderr:write(string.format("OP: %s %s\n", k, v))
+    -- end
+
+    local recce = ...
+
+    recce.op_fn_key = {}
+
+    function op_fn_create(name, fn) 
+        local ref = recce:ref(fn);
+        recce.op_fn_key[name] = ref;
+    end
+
+    recce.rule_semantics = {}
+    recce.token_semantics = {}
+    recce.nulling_semantics = {}
+    recce.nulling_semantics.default
+        = marpa.array.from_list(marpa.ops.result_is_undef,0)
+    recce.token_semantics.default
+        = marpa.array.from_list(marpa.ops.result_is_token_value,0)
+    recce.rule_semantics.default
+        = marpa.array.from_list(marpa.ops.result_is_undef, 0)
+    -- print( recce.nulling_semantics.default )
+    -- io.stderr:write(string.format("len: %s\n", #(recce.nulling_semantics.default)))
+    -- io.stderr:write(string.format("#0: %s\n", recce.nulling_semantics.default[0]))
+    -- io.stderr:write(string.format("#1: %s\n", recce.nulling_semantics.default[1]))
+
+    op_fn_create("debug", function (...)
+        local recce, type, result_ix, rule_id, arg_n = ...
+        print([[OP_LUA:]], recce, type, result_ix, rule_id, arg_n)
+        for k,v in pairs(recce) do
+            print(k, v)
+        end
+        mt = debug.getmetatable(recce)
+        print([[=== metatable ===]])
+        for k,v in pairs(mt) do
+            print(k, v)
+        end
+    end)
+
+    -- print("stack len:", marpa.sv.top_index(recce:stack()))
+
+    op_fn_create("result_is_undef", function (...)
+        local recce, type, result_ix = ...
+        local stack = recce:stack()
+        stack[result_ix] = marpa.sv.lua_nil()
+        marpa.sv.fill(stack, result_ix)
+        return 0
+    end)
+
+END_OF_LUA
+1;
--- cpan/lib/Marpa/R3/SLR.pm
+++ cpan/lib/Marpa/R3/SLR.pm
@@ -297,59 +297,7 @@ sub Marpa::R3::Scanless::R::new {
     Marpa::R3::Internal::Scanless::convert_libmarpa_events($slr);
 
     # Stuff in Lua
-
-    $slr->exec_string(<<'END_OF_LUA');
-    -- for k,v in pairs(marpa.ops)
-    -- do io.stderr:write(string.format("OP: %s %s\n", k, v))
-    -- end
-
-    local recce = ...
-
-    recce.op_fn_key = {}
-
-    function op_fn_create(name, fn) 
-        local ref = recce:ref(fn);
-        recce.op_fn_key[name] = ref;
-    end
-
-    recce.rule_semantics = {}
-    recce.token_semantics = {}
-    recce.nulling_semantics = {}
-    recce.nulling_semantics.default
-        = marpa.array.from_list(marpa.ops.result_is_undef,0)
-    recce.token_semantics.default
-        = marpa.array.from_list(marpa.ops.result_is_token_value,0)
-    recce.rule_semantics.default
-        = marpa.array.from_list(marpa.ops.result_is_undef, 0)
-    -- print( recce.nulling_semantics.default )
-    -- io.stderr:write(string.format("len: %s\n", #(recce.nulling_semantics.default)))
-    -- io.stderr:write(string.format("#0: %s\n", recce.nulling_semantics.default[0]))
-    -- io.stderr:write(string.format("#1: %s\n", recce.nulling_semantics.default[1]))
-
-    op_fn_create("debug", function (...)
-        local recce, type, result_ix, rule_id, arg_n = ...
-        print([[OP_LUA:]], recce, type, result_ix, rule_id, arg_n)
-        for k,v in pairs(recce) do
-            print(k, v)
-        end
-        mt = debug.getmetatable(recce)
-        print([[=== metatable ===]])
-        for k,v in pairs(mt) do
-            print(k, v)
-        end
-    end)
-
-    -- print("stack len:", marpa.sv.top_index(recce:stack()))
-
-    op_fn_create("result_is_undef", function (...)
-        local recce, type, result_ix = ...
-        local stack = recce:stack()
-        stack[result_ix] = marpa.sv.lua_nil()
-        marpa.sv.fill(stack, result_ix)
-        return 0
-    end)
-
-END_OF_LUA
+    $slr->exec_string($Marpa::R3::Lua::value_init);
 
     return $slr;
 } ## end sub Marpa::R3::Scanless::R::new
