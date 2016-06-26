7fadb072154de4d8d7ee395ec520db956945f78b
Jeffrey Kegler <jeffreykegler@jeffreykegler.com>
2016-06-25 21:14:47 -0700

    Work on adding Lua: t++

--- cpan/xlua/inspect.lua
+++ cpan/xlua/inspect.lua
@@ -1,26 +1,3 @@
-# Marpa::R3 is Copyright (C) 2016, Jeffrey Kegler.
-#
-# This module is free software; you can redistribute it and/or modify it
-# under the same terms as Perl 5.10.1. For more details, see the full text
-# of the licenses in the directory LICENSES.
-#
-# This program is distributed in the hope that it will be
-# useful, but it is provided “as is” and without any express
-# or implied warranties. For details, see the full text of
-# of the licenses in the directory LICENSES.
-
-package Marpa::R3::Lua::Inspect;
-
-use 5.010001;
-use warnings;
-use strict;
-
-use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '4.001_013';
-$STRING_VERSION = $VERSION;
-$VERSION        = eval $VERSION;
-
-$Marpa::R3::Lua::Inspect::load = <<'END_OF_LUA';
 local do_load = load([==========[
 local inspect ={
   _VERSION = 'inspect.lua 3.1.0',
@@ -365,8 +342,5 @@ return inspect
 
 ]==========]);
 inspect = do_load()
-END_OF_LUA
 
-1;
-
-# vim: set expandtab shiftwidth=4:
+-- vim: set expandtab shiftwidth=4:
