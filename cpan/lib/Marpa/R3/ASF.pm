a097922c800da9bcac87841cf56100b95924aff7
Jeffrey Kegler <jeffreykegler@jeffreykegler.com>
2016-06-28 19:40:45 -0700

    Bump version to 4.001_015

--- cpan/lib/Marpa/R3/ASF.pm
+++ cpan/lib/Marpa/R3/ASF.pm
@@ -17,7 +17,7 @@ use warnings;
 no warnings qw(recursion);
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '4.001_014';
+$VERSION        = '4.001_015';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- cpan/lib/Marpa/R3/Common.pm
+++ cpan/lib/Marpa/R3/Common.pm
@@ -19,7 +19,7 @@ use strict;
 use English qw( -no_match_vars );
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '4.001_014';
+$VERSION        = '4.001_015';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
