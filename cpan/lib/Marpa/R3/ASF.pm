bd2f5bf2f94e152c5759e10434e4bfef10fa4385
Jeffrey Kegler <jeffreykegler@jeffreykegler.com>
2016-08-07 11:07:55 -0700

    Bump version to 4.001_020: t++

--- cpan/lib/Marpa/R3/ASF.pm
+++ cpan/lib/Marpa/R3/ASF.pm
@@ -17,7 +17,7 @@ use warnings;
 no warnings qw(recursion);
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '4.001_019';
+$VERSION        = '4.001_020';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
