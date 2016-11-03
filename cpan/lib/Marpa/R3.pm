a04fdad90386fcea152676d3316d5747b8d62488
Jeffrey Kegler <jeffreykegler@jeffreykegler.com>
2016-11-02 21:25:56 -0700

    Bump version to 4.001_023

--- cpan/lib/Marpa/R3.pm
+++ cpan/lib/Marpa/R3.pm
@@ -16,7 +16,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION        = '4.001_022';
+$VERSION        = '4.001_023';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- cpan/lib/Marpa/R3/Internal.pm
+++ cpan/lib/Marpa/R3/Internal.pm
@@ -20,7 +20,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '4.001_022';
+$VERSION        = '4.001_023';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
