27aeb3855e8ddd6a92dfdeca73ab29d15bd05275
Jeffrey Kegler <jeffreykegler@jeffreykegler.com>
2016-12-07 11:24:53 -0800

    Refactoring recognizer: t++

--- cpan/lib/Marpa/R3/SLG.pm
+++ cpan/lib/Marpa/R3/SLG.pm
@@ -16,7 +16,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '4.001_026';
+$VERSION        = '4.001_027';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
