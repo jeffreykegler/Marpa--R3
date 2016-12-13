b0201147f6d63352cf9e639394d3aae3163998ce
Jeffrey Kegler <jeffreykegler@jeffreykegler.com>
2016-12-13 12:35:43 -0800

    Refactoring recognizer: t++

--- cpan/lib/Marpa/R3.pm
+++ cpan/lib/Marpa/R3.pm
@@ -80,10 +80,6 @@ require Marpa::R3::Thin::G;
 ( $version_result = version_ok($Marpa::R3::Thin::G::VERSION) )
     and die 'Marpa::R3::Thin::G::VERSION ', $version_result;
 
-require Marpa::R3::Thin::R;
-( $version_result = version_ok($Marpa::R3::Thin::R::VERSION) )
-    and die 'Marpa::R3::Thin::R::VERSION ', $version_result;
-
 require Marpa::R3::Trace::G;
 ( $version_result = version_ok($Marpa::R3::Trace::G::VERSION) )
     and die 'Marpa::R3::Trace::G::VERSION ', $version_result;
