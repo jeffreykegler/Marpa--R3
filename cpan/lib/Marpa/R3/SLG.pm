9bb23a649ba5f5ce33d74765e15a49680471805a
Jeffrey Kegler <jeffreykegler@jeffreykegler.com>
2017-06-22 20:10:58 -0700

    Clean up symbol/rule methods: t++

--- cpan/lib/Marpa/R3/SLG.pm
+++ cpan/lib/Marpa/R3/SLG.pm
@@ -1331,7 +1331,7 @@ sub add_G1_user_rule {
           <<'END_OF_LUA', 's', $xbnf_name);
     local grammar, xbnf_name = ...
     local default_rank = grammar.g1:default_rank()
-    local xbnf_id = grammar.g1.xbnfs[xbnf_name].id
+    local xbnf_id = grammar.g1.xbnfs[xbnf_name].new_id
     return default_rank, xbnf_id
 END_OF_LUA
 
@@ -1442,7 +1442,7 @@ END_OF_LUA
         local g1g = slg.g1
         g1g:rule_null_high_set(irl_id, ranking_is_high)
         g1g:rule_rank_set(irl_id, rank)
-        local xbnf = slg.g1.xbnfs[xbnf_id]
+        local xbnf = slg.xbnfs[xbnf_id]
         local irl = slg.g1.irls[irl_id]
         irl.xbnf = xbnf
         -- right now, the action & mask of an irl
@@ -1507,7 +1507,7 @@ sub add_L0_user_rule {
     -- io.stderr:write('xbnf_name: ', inspect(xbnf_name), '\n')
     local xbnf_id = -1
     if #xbnf_name > 0 then
-        xbnf_id = slg.l0.xbnfs[xbnf_name].id
+        xbnf_id = slg.l0.xbnfs[xbnf_name].new_id
     end
     return default_rank, xbnf_id
 END_OF_LUA
