4ce832fe355853adf2a97bc069a7191f873118ab
Jeffrey Kegler <jeffreykegler@jeffreykegler.com>
2017-06-28 20:26:21 -0700

    Clean up symbol/rule methods: t++

--- cpan/kollos/toc.lua
+++ cpan/kollos/toc.lua
@@ -21,30 +21,11 @@ while true do
   local href = lowered:gsub('[%s%c%p]+', '-')
   href = href:gsub('^[-]+', '')
   href = href:gsub('[-]+$', '')
-  do
-    local href_id = hrefs[href]
-    if not href_id then
-      hrefs[href] = ''
-      goto HREF_FOUND
-    end
-    local base = href
-    if href_id ~= '' then
-         base = string.sub(href, 1, -(2+#href_id))
-    end
-    local id_num = math.tointeger(href_id)
-    if not id_num then id_num = 0 end
-    while true do
-      local new_href_id = tostring(id_num+1)
-      local new_href = base .. '-' .. new_href_id
-      if not hrefs[new_href] then
-	 hrefs[new_href] = new_href_id
-	 href = new_href
-	 goto HREF_FOUND
-      end
-      id_num = id_num + 1
-    end
+  local href_id = hrefs[href]
+  if href_id then
+     href = href .. '-DUP'
   end
-  ::HREF_FOUND::
+  hrefs[href] = true
   io.stdout:write(string.format('%s* [%s](#%s)\n', string.rep('  ', depth-2), title, href))
   ::NEXT_LINE::
 end
