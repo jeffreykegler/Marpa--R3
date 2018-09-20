5a0677526f50a19601dad6456f96ba83a671103b
Jeffrey Kegler <jeffreykegler@cpan.org>
2018-09-20 14:11:32 -0400

    Bumper version to 4.001_053

--- cpan/lib/Marpa/R3/Glade.pm
+++ cpan/lib/Marpa/R3/Glade.pm
@@ -17,7 +17,7 @@ use warnings;
 no warnings qw(recursion);
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '4.001_052';
+$VERSION        = '4.001_053';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- cpan/lib/Marpa/R3/Internal.pm
+++ cpan/lib/Marpa/R3/Internal.pm
@@ -20,7 +20,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '4.001_052';
+$VERSION        = '4.001_053';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
